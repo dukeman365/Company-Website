233b035ad5f3ae5543c8550b1bc1a587f4e0cf41
Robinson Cook <robinsoncook@gmail.com>
2019-01-06 01:45:11 -0600

    responsive text

--- server/api/routes/post_id.js
+++ server/api/routes/post_id.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-
+  ``
 
 //import models
 var Post = require('../../../database/models/post');
