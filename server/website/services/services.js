198b62565aeef7bbbb573d2620fdaacfb7813b16
Robinson Cook <robinsoncook@gmail.com>
2019-01-21 21:38:10 -0600

    Beginning of Services

--- server/website/services/services.js
+++ server/website/services/services.js
@@ -2,5 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.route('/services')
+  .get(function(req, res) {
+    res.render('services', {
+      layout: 'servicesLayout'
+    });
+  })
 
-module.exports=router;
+module.exports = router;
