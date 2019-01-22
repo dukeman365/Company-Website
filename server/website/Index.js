0f0678bcac2f4cd0474566470d56952f51182705
Robinson Cook <robinsoncook@gmail.com>
2019-01-21 19:30:22 -0600

    Fix

--- server/website/Index.js
+++ server/website/Index.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var Post = require('../../database/models/post')
 var post_id=require('./blog/post_id')
 var contact=require('./contact/contact')
-
+var services=require('./services/services')
 //Route for Index page
 router.get('/', function(req, res) {
   res.render('home', {
@@ -81,6 +81,7 @@ router.get('/blog/:post_id', function(req, res) {
   })
 })
 
+router.use('/',services)
 router.use('/',contact);
 router.use('/',post_id);
 module.exports = router;
